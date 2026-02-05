3f2eb6f76656d9f10bd2e9e75e9ad2014c93fd00
sirtheprogrammer <sirtheprogrammer@gmail.com>
2026-02-05 05:48:26 +0300

    add simple displaying logo

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,11 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import natakahiiLogo from './assets/natakahii.png'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-       
-      </div>
-      <p className="read-the-docs">
-       Welcome to natakahii
-      </p>
-    </>
+    <div className="app-container">
+      <img src={natakahiiLogo} alt="Nataka Hii Logo" className="logo" />
+    </div>
   )
 }
 
